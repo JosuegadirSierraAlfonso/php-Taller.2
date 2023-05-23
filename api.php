4298546ea1c55240a690ea3b774256620332ca3b
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-23 17:15:37 -0500

    feat: :rocket: Exercise 7
    Program that requests the entry of the name and price of an item and the quantity carried by the customer. Show what the buyer must pay on your bill

--- api.php
+++ api.php
@@ -1,78 +1,63 @@
 <?php
-    /* Build the algorithm in Javascript for a program
-    for any number of students reading the name,
-    the sex and the final grade and find the student with the highest
-    grade and the student with the lowest grade and how many were
-    men and how many women.*/
+    /* Program that requests the entry of the name and price of an item and the
+    quantity carried by the customer. Show what the buyer must pay
+    on your bill.*/
     header("Content-Type: application/json; charset:UTF-8");
     $_DATA = json_decode(file_get_contents("php://input"), true);
     $METHOD = $_SERVER["REQUEST_METHOD"];
 
     try {
         $res = match ($METHOD) {
-            "POST" => algorithm(...$_DATA)
+            "POST" => algorithm()
         };
     } catch (\Throwable $th) {
         $res = "Error";
-    }
+    };
+    
     function algorithm()
-{
-    global $_DATA;
-
-    $names = array_column($_DATA, 'name');
-    $notes = array_column($_DATA, 'note');
-    $registration = $_DATA;
-
-    foreach ($names as $name) {
-        if (!is_string($name) || empty(trim($name)) || !preg_match('/^[A-Za-z]+$/', $name)) {
-            $res = "Error, el nombre no puede ser numerico ni contener numeros";
-            echo json_encode($res, JSON_PRETTY_PRINT);
-            exit;
+    {
+        global $_DATA;
+        $names = array_column($_DATA, 'nameP');
+        $prices = array_column($_DATA, 'price');
+        $amounts = array_column($_DATA, 'amount');
+        $registration = $_DATA;
+    
+        foreach ($names as $name) {
+            if (!is_string($name) || empty(trim($name)) || !preg_match('/^[A-Za-z]+$/', $name)) {
+                $res = "Error, el nombre no puede ser numerico ni contener numeros";
+                echo json_encode($res, JSON_PRETTY_PRINT);
+                exit;
+            }
         }
-    }
-
-    foreach ($notes as $note) {
-        if (!is_numeric($note)) {
-            $res = "Error: La nota debe ser numerica.";
-            echo json_encode($res, JSON_PRETTY_PRINT);
-            exit;
+        foreach ($prices as $price) {
+            if (!is_numeric($price)) {
+                $res = "Error: El precio debe ser numeric.";
+                echo json_encode($res, JSON_PRETTY_PRINT);
+                exit;
+            }
         }
-    }
-
-    $counterM = 0;
-    $counterF = 0;
-
-    foreach ($registration as $registro) {
-        $gender = $registro["gender"];
-        if ($gender === "m") {
-            $counterM++;
-        } elseif ($gender === "f") {
-            $counterF++;
+        foreach ($amounts as $amount) {
+            if (!is_numeric($amount)) {
+                $res = "Error: La cantidad debe ser numerica.";
+                echo json_encode($res, JSON_PRETTY_PRINT);
+                exit;
+            }
         }
+    
+        $subtotally = array_map(function ($price, $amount) {
+            return $price * $amount;
+        }, $prices, $amounts);
+    
+        $totally = array_sum($subtotally);
+        $productsSubtotally = array_combine($names, $subtotally);
+    
+        $message = array(
+            "Lista de Productos" => $_DATA,
+            "Productos" => $productsSubtotally,
+            "Total" => $totally
+        );
+        echo json_encode($message, JSON_PRETTY_PRINT);
     }
-
-    $maxNote = max($notes);
-    $maxNoteIndex = array_search($maxNote, $notes);
-    $minNote = min($notes);
-    $minNotaIndex= array_search($minNote, $notes);
-
-    $personMaxNote = array(
-        "Name" => $names[$maxNoteIndex],
-        "Note" => $maxNote
-    );
-    $personMinNote = array(
-        "Name" => $names[$minNoteIndex],
-        "Note" => $minNote
-    );
-
-    $message = array(
-        "Students list" => $_DATA,
-        "person with best score" => $personMaxNote,
-        "person with the lowest score" => $personMinNote,
-        "male students" => $counterM,
-        "female students" => $counterF
-    );
-    echo json_encode($message, JSON_PRETTY_PRINT);
-}
+    
     
 ?>
