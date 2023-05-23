aa0eebd159b03ec449ff32f7324a514d39a8f298
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-23 15:32:17 -0500

    feat: :sparkles: Exercise 3.1
    validations added

--- api.php
+++ api.php
@@ -4,25 +4,30 @@
     header("Content-Type: application/json; charset:UTF-8");
     $_DATA = json_decode(file_get_contents("php://input"), true);
     $METHOD = $_SERVER["REQUEST_METHOD"];
+    if (is_numeric($_DATA["note1"]) && is_numeric($_DATA["note2"])) {
+        $note1 = $_DATA["note1"];
+        $note2 = $_DATA["note2"];
 
-    function validate($arg){
-        return ("$arg volts") ;
-    }
-    function algorithm(float $note1,float $note2){   
-        $voltage = $note1 * $note2;
-        return $voltage;
-    }
-
+        function algorithm(float $note1,float $note2){   
+            $voltage = $note1 * $note2;
+            return $voltage;
+        }
+    
+        $voltage = algorithm($note1, $note2);
+    
     try {
-    $res = match($METHOD){
-        "POST" => algorithm(...$_DATA)
-    };
-    }catch (\Throwable $th) {
-    $res = "ERROR";
+        $res = match($METHOD){
+            "POST" => algorithm(...$_DATA)
+        };
+        }catch (\Throwable $th) {
+        $res = "ERROR";
 
+        };
+        $message = (array) [
+            "Voltage" => "$voltage voltios"
+        ];
+        echo json_encode( $message,JSON_PRETTY_PRINT);
+    } else {
+        echo "Los valores deben ser numericos";
     };
-    $message = (array) [
-        "Voltage"=> validate($res),
-    ];
-    echo json_encode( $message,JSON_PRETTY_PRINT);
 ?>
