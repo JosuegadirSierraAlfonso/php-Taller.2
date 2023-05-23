fbb7d4919df17410f27a5ff54a8fef4fcd1f0ae9
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-23 16:44:43 -0500

    feat: :rocket: Exercise 6
    Build the algorithm in Javascript for a program     for any number of students reading the name,     the sex and the final grade and find the student with the highest     grade and the student with the lowest grade and how many were     men and how many women

--- api.php
+++ api.php
@@ -1,40 +1,58 @@
 <?php
-    /* Build the algorithm that reads two numbers from the keyboard,
-If the first is greater than the second, report their sum and
-difference, otherwise, inform the product and the
-division of the first by the second */
+    /* Build the algorithm in Javascript for a program
+    for any number of students reading the name,
+    the sex and the final grade and find the student with the highest
+    grade and the student with the lowest grade and how many were
+    men and how many women.*/
     header("Content-Type: application/json; charset:UTF-8");
     $_DATA = json_decode(file_get_contents("php://input"), true);
     $METHOD = $_SERVER["REQUEST_METHOD"];
 
-    if (is_numeric($_DATA["num1"]) && is_numeric($_DATA["num2"])) {
-        $num1 = $_DATA["num1"];
-        $num2 = $_DATA["num2"];
-        $totally = $num1 + $num2;
-        $product = $num1 * $num2;
-        $less = $num2 - $num1;
-        $division = $num2 / $num1;
-    
-        if ($num1 > $num2) {
-            $message = array(
-                "message" => "The sum is $totally and its product is $product"
-            );
-            echo json_encode($message, JSON_PRETTY_PRINT);
-        } else {
-            $message = array(
-                "message" => "The less is $less and its division is $division"
-            );
-            echo json_encode($message, JSON_PRETTY_PRINT);
+    $names = array_column($_DATA, 'name');
+    $notes = array_column($_DATA, 'note');
+    $registration = $_DATA;
+
+    $counterM = 0;
+    $counterF = 0;
+
+    foreach ($registration as $registro) {
+        $gender = $registro["gender"];
+        if ($gender === "m") {
+            $counterM++;
+        } elseif ($gender === "f") {
+            $counterF++;
         }
-        try {
-            $res = match ($METHOD) {
-                "POST" => algoritmo(...$_DATA)
-            };
-        } catch (\Throwable $th) {
-            $res = "Error";
-        }
-    
-    }else{
-        echo "Los valores deben ser numericos";
     }
+
+    $maxNote = max($notes);
+    $maxNoteIndex = array_search($maxNote, $notes);
+    $minNote = min($notes);
+    $minNotaIndex= array_search($minNote, $notes);
+
+    $personMaxNote = array(
+        "Name" => $names[$maxNoteIndex],
+        "Note" => $maxNote
+    );
+    $personMinNote = array(
+        "Name" => $names[$minNoteIndex],
+        "Note" => $minNote
+    );
+
+    try {
+        $res = match ($METHOD) {
+            "POST" => algoritmo(...$_DATA)
+        };
+    } catch (\Throwable $th) {
+        $res = "Error";
+    }
+
+    $message = array(
+        "Students list" => $_DATA,
+        "person with best score" => $personMaxNote,
+        "person with the lowest score" => $personMinNote,
+        "male students" => $counterM,
+        "female students" => $counterF
+    );
+
+    echo json_encode($message, JSON_PRETTY_PRINT);
 ?>
--- main.js
+++ main.js
@@ -4,13 +4,19 @@ let config = {
     headers: myHeaders,
 };
 
-myFormulario.addEventListener("submit", async(e)=>{
-    e.preventDefault();
+let registration = [];
 
-    config.method = "POST";
-    let data = Object.fromEntries(new FormData(e.target));
-    config.body = JSON.stringify(data);
-    let res = await (await fetch("api.php", config)).text();
-    document.querySelector("pre").innerHTML = res;
-    console.log(res);
-})
+myFormulario.addEventListener("submit", async(e) => {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+    let data = Object.fromEntries(formData);
+    registration.push(data); 
+    let res = await (await fetch("api.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(registration)
+    })).json();
+    document.querySelector("#resul").textContent = JSON.stringify(res, null, 2);
+});
