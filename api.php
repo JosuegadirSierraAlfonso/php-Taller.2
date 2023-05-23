ad22dd2333d8a12080d30422d0b15764cf56ad7c
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-23 16:08:13 -0500

    feat: :rocket: Exercise 5
    Build the algorithm that reads two numbers from the keyboard, If the first is greater than the second, report their sum and difference, otherwise, inform the product and the division of the first by the second

--- api.php
+++ api.php
@@ -1,49 +1,40 @@
 <?php
-    /* Build the algorithm that requests the name and age of 3
-    people and determine the name of the oldest person. */
+    /* Build the algorithm that reads two numbers from the keyboard,
+If the first is greater than the second, report their sum and
+difference, otherwise, inform the product and the
+division of the first by the second */
     header("Content-Type: application/json; charset:UTF-8");
     $_DATA = json_decode(file_get_contents("php://input"), true);
     $METHOD = $_SERVER["REQUEST_METHOD"];
 
-    $names = array_column($_DATA, 'name');
-    $ages = array_column($_DATA, 'age');
-    $registration = $_DATA; 
-
-    foreach ($names as $name) {
-        if (!is_string($name) || empty(trim($name)) || !preg_match('/^[A-Za-z]+$/', $name)) {
-            $res = "Error, The name cannot be numeric or contain numbers.";
-            echo json_encode($res, JSON_PRETTY_PRINT);
-            exit;
+    if (is_numeric($_DATA["num1"]) && is_numeric($_DATA["num2"])) {
+        $num1 = $_DATA["num1"];
+        $num2 = $_DATA["num2"];
+        $totally = $num1 + $num2;
+        $product = $num1 * $num2;
+        $less = $num2 - $num1;
+        $division = $num2 / $num1;
+    
+        if ($num1 > $num2) {
+            $message = array(
+                "message" => "The sum is $totally and its product is $product"
+            );
+            echo json_encode($message, JSON_PRETTY_PRINT);
+        } else {
+            $message = array(
+                "message" => "The less is $less and its division is $division"
+            );
+            echo json_encode($message, JSON_PRETTY_PRINT);
         }
-    }
-    foreach ($ages as $age) {
-        if (!is_numeric($age)) {
-            $res = "Error: Age must be numeric.";
-            echo json_encode($res, JSON_PRETTY_PRINT);
-            exit;
+        try {
+            $res = match ($METHOD) {
+                "POST" => algoritmo(...$_DATA)
+            };
+        } catch (\Throwable $th) {
+            $res = "Error";
         }
+    
+    }else{
+        echo "Los valores deben ser numericos";
     }
-
-    $maxAge = max($ages);
-    $maxAgeIndex = array_search($maxAge, $ages);
-
-    $personMaxAge = array(
-        "Name" => $names[$maxAgeIndex],
-        "Age" => $maxAge
-    );
-
-    try {
-        $res = match($METHOD){
-            "POST" => algoritmo(...$_DATA)
-        };
-    } catch (\Throwable $th) {
-        $res = "Error";
-    }
-
-    $message = array(
-        "Students list" => $_DATA,
-        "Older Student" => $personMaxAge,
-    );
-
-    echo json_encode($message, JSON_PRETTY_PRINT);
 ?>
--- main.js
+++ main.js
@@ -3,20 +3,14 @@ let myHeaders = new Headers({"Content-Type": "application/json"});
 let config = {
     headers: myHeaders,
 };
-let registration = [];
 
 myFormulario.addEventListener("submit", async(e)=>{
     e.preventDefault();
 
-    let formData = new FormData(e.target);
-    let data = Object.fromEntries(formData);
-    registration.push(data);
-    let res = await (await fetch("api.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(registration)
-    })).json();
-    document.querySelector("#resul").textContent = JSON.stringify(res, null, 2);
+    config.method = "POST";
+    let data = Object.fromEntries(new FormData(e.target));
+    config.body = JSON.stringify(data);
+    let res = await (await fetch("api.php", config)).text();
+    document.querySelector("pre").innerHTML = res;
+    console.log(res);
 })
