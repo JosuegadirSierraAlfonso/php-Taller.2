e58e658cd742468d9f1121b9818044e47fc56e2d
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-24 15:00:38 -0500

    feat: :rocket: Exercise 10
    Develop a cyclical program that captures a piece of data number each time, and accumulate them. The program will      stops when the user types a zero. The program must      show: THE SUM OF THE VALUES, THE VALUE OF THE AVERAGE, HOW MANY VALUES WERE ENTERED, MAYOR VALUE AND LESS VALUE

--- api.php
+++ api.php
@@ -1,11 +1,10 @@
 <?php
-    /* N athletes have advanced to triple jump finals at the games
-    2022 Women's Olympians. Design a program that asks for
-    keyboard the names of each finalist athlete and in turn, their
-    marks of the jump in meters. Inform the name of the athlete
-    champion to keep the gold medal and if she broke
-    record, report the payment that will be 500 million. the record
-    It is at 15.50 meters.*/
+    /* Develop a cyclical program that captures a piece of data
+     number each time, and accumulate them. The program will
+     stops when the user types a zero. The program must
+     show: THE SUM OF THE VALUES, THE VALUE OF THE
+     AVERAGE, HOW MANY VALUES WERE ENTERED, MAYOR
+     VALUE AND LESS VALUE.*/
     header("Content-Type: application/json; charset:UTF-8");
     $_DATA = json_decode(file_get_contents("php://input"), true);
     $METHOD = $_SERVER["REQUEST_METHOD"];
@@ -20,41 +19,38 @@
 
     function algorithm(){
         global $_DATA;
-        $names = array_column($_DATA, 'name');
-    $brands = array_column($_DATA, 'brand');
+        $numbers = array_column($_DATA, 'num');
     $registration = $_DATA;
 
-    foreach ($names as $name) {
-        if (!is_string($name) || empty(trim($name)) || !preg_match('/^[A-Za-z]+$/', $name)) {
-            $res = "Error, The name cannot be numeric or contain numbers.";
+    if (in_array(0, $numbers)) {
+        $res = "You're out";
+        echo json_encode($res, JSON_PRETTY_PRINT);
+        exit;
+    }
+
+    foreach ($numbers as $number) {
+        if (!is_numeric($number)) {
+            $res = "Error: Values must be numeric.";
             echo json_encode($res, JSON_PRETTY_PRINT);
             exit;
         }
-    };
+    }
 
-    foreach ($brands as $brand) {
-        if (!is_numeric($brand)) {
-            $res = "Error: The Mark must be numeric.";
-            echo json_encode($res, JSON_PRETTY_PRINT);
-            exit;
-        }
-    };
-
-    $maxBrand = max($brands);
-    $maxBrandIndex = array_search($maxBrand, $brands);
-
-    $personMaxBrand = array(
-        "name" => $names[$maxBrandIndex],
-        "brand" => $maxBrand
-    );
+    $totally = array_sum($numbers);
+    $count = count($numbers);
+    $average = $totally / $count;
+    $maxVal = max($numbers);
+    $minVal = min($numbers);
 
     $message = array(
-        "Athlete List" => $_DATA,
-        "Athlete With the Greatest Mark" => $personMaxBrand,
+        "Entered Numbers" => $_DATA,
+        "Total amount" => $totally,
+        "Number of Data Entered" => $count,
+        "Total average" => $average,
+        "The number with the greatest value is:" => $maxVal,
+        "The number with the least value is:" => $minVal,
     );
-    if ($maxBrand > 15.50) {
-        $message["Congratulations!!!"] = "¡You've got it! You have won 500 Million for breaking the record.";
-    };
+
     echo json_encode($message, JSON_PRETTY_PRINT);
-    };
+    }
 ?>
