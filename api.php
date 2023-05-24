ca46a0214f52f6ffee83c4b448a3ac824e129d3b
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-24 14:52:25 -0500

    feat: :rocket: Exercise 9
    N athletes have advanced to triple jump finals at the games 2022 women's olympians

--- api.php
+++ api.php
@@ -1,48 +1,60 @@
 <?php
-    /* Program that enters by keyboard:
-    a. the value of the side of a square to display on the screen the
-    perimeter of the same
-    b. the base and height of a rectangle to show the area of the
-    same*/
+    /* N athletes have advanced to triple jump finals at the games
+    2022 Women's Olympians. Design a program that asks for
+    keyboard the names of each finalist athlete and in turn, their
+    marks of the jump in meters. Inform the name of the athlete
+    champion to keep the gold medal and if she broke
+    record, report the payment that will be 500 million. the record
+    It is at 15.50 meters.*/
     header("Content-Type: application/json; charset:UTF-8");
     $_DATA = json_decode(file_get_contents("php://input"), true);
     $METHOD = $_SERVER["REQUEST_METHOD"];
 
-    if (is_numeric($_DATA['square']) && is_numeric($_DATA['heigth']) && is_numeric($_DATA['base'])) {
-        $square = $_DATA['square'];
-        $heigth = $_DATA['heigth'];
-        $base = $_DATA['base'];
+    try {
+        $res = match($METHOD){
+            "POST" => algorithm(...$_DATA)
+        };
+    } catch (\Throwable $th) {
+        $res = "Error";
+    };
 
-        function perimeter(float $square)
-        {
-            $perimeter = $square * 4;
-            return $perimeter;
+    function algorithm(){
+        global $_DATA;
+        $names = array_column($_DATA, 'name');
+    $brands = array_column($_DATA, 'brand');
+    $registration = $_DATA;
+
+    foreach ($names as $name) {
+        if (!is_string($name) || empty(trim($name)) || !preg_match('/^[A-Za-z]+$/', $name)) {
+            $res = "Error, The name cannot be numeric or contain numbers.";
+            echo json_encode($res, JSON_PRETTY_PRINT);
+            exit;
         }
+    };
 
-        function area(float $heigth, float $base)
-        {
-            $area = $heigth * $base;
-            return $area;
+    foreach ($brands as $brand) {
+        if (!is_numeric($brand)) {
+            $res = "Error: The Mark must be numeric.";
+            echo json_encode($res, JSON_PRETTY_PRINT);
+            exit;
         }
+    };
 
-        try {
-            $res = match ($METHOD) {
-                "POST" => [
-                    "Perimeter of the square is:" => perimeter($square),
-                    "Height of the rectangle is:" => area($heigth, $base),
-                ],
-            };
-        } catch (\Throwable $th) {
-            $res = "Error";
-        }
+    $maxBrand = max($brands);
+    $maxBrandIndex = array_search($maxBrand, $brands);
 
-        $message = array(
-            "Information" => $_DATA,
-            "Result" => $res,
-        );
+    $personMaxBrand = array(
+        "name" => $names[$maxBrandIndex],
+        "brand" => $maxBrand
+    );
 
-        echo json_encode($message, JSON_PRETTY_PRINT);
-    } else {
-        echo "Values must be numeric.";
-    }
+    $message = array(
+        "Athlete List" => $_DATA,
+        "Athlete With the Greatest Mark" => $personMaxBrand,
+    );
+    if ($maxBrand > 15.50) {
+        $message["Congratulations!!!"] = "¡You've got it! You have won 500 Million for breaking the record.";
+    };
+    echo json_encode($message, JSON_PRETTY_PRINT);
+    };
 ?>
--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 let myFormulario = document.querySelector("#myFormulario");
-let myHeaders = new Headers({"Content-Type": "applicatio/json"});
+let myHeaders = new Headers({"Content-Type": "application/json"});
 let config = {
     headers: myHeaders,
-}
-myFormulario.addEventListener("submit", async(e)=>{
+};
+
+let registration = [];
+
+myFormulario.addEventListener("submit", async(e) => {
     e.preventDefault();
-    config.method = "POST";
-    let data = Object.fromEntries(new FormData(e.target));
-    config.body = JSON.stringify(data);
-    let res = await (await fetch("api.php", config)).text();
-    document.querySelector("pre").innerHTML=res;
-    console.log(res);
-    
-})
+    let formData = new FormData(e.target);
+    let data = Object.fromEntries(formData);
+    registration.push(data); 
+    let res = await (await fetch("api.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(registration)
+    })).json();
+    document.querySelector("#resul").textContent = JSON.stringify(res, null, 2);
+});
